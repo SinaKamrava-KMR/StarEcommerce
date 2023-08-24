8b763788271a880352b7c5a465038f8e7b840081
Sina Kamrava <sinakamrava3118@gmail.com>
2023-08-25 01:02:35 +0330

    feat : implement slider pagiantion

--- src/components/home/SliderCircle.jsx
+++ src/components/home/SliderCircle.jsx
@@ -5,31 +5,28 @@ const SliderCircleWrapper = styled.div`
   width: ${(props) => (props.expand ? "100%" : `350px`)};
   height: ${(props) => (props.expand ? "100%" : `350px`)};
   border-radius: ${(props) =>
-    props.expand ? 0 : ` 31% 69% 23% 77% / 66% 18% 82% 34%
-                    `};
+    props.expand ? 0 : ` 31% 69% 23% 77% / 66% 18% 82% 34%`};
   margin-inline: auto;
-  transition: all .3s ease-in-out;
+  transition: all 0.3s ease-in-out;
   overflow: hidden;
   position: absolute;
   right: ${(props) => (props.expand ? 0 : `20%`)};
   bottom: ${(props) => (props.expand ? 0 : `20%`)};
-  background-color: #d5fffea1;
+  background-color: ${(props) => props.color};
   /* background-image: linear-gradient(to right, #fdb4c166 0%, #fba3b466 19%, #ffb4b867 60%, #ffcbc35b 100%); */
 `;
 
-const SliderCircle = ({ expand }) => {
+const SliderCircle = ({ expand, color }) => {
   return (
-    <SliderCircleWrapper expand={expand}>
-     
-    </SliderCircleWrapper>
+    <SliderCircleWrapper
+      color={`${color}41`}
+      expand={expand}
+    ></SliderCircleWrapper>
   );
 };
 
 SliderCircle.propTypes = {
-  onSlideChange: PropTypes.func,
-  onActiveSlide: PropTypes.func,
-  effect: PropTypes.string,
-  autoPlay: PropTypes.object,
+  color: PropTypes.string,
   expand: PropTypes.bool,
 };
 
---
+++ src/components/slider/PaginationSlider.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import { styled } from "styled-components";
+
+const Wrapper = styled.div`
+  position: absolute;
+  left: 10%;
+  bottom: 10%;
+  color: #fff;
+  display: flex;
+  gap: 1.5rem;
+  align-items: center;
+`;
+
+const Circle = styled.div`
+  width: 8px;
+  height: 8px;
+  background-color: ${(props) => (props.active ? props.color : "#9f9f9f")};
+  transform: ${props=>props.active?`scale(1.5)`:"none"};
+  border-radius: 50%;
+  transition: all 0.5s ease-in;
+`;
+
+const PaginationSlider = ({ activeIndex, length, colors = [] }) => {
+  return (
+    <Wrapper>
+      {Array.from({ length }).map((_, i) =>
+        activeIndex === i ? (
+          <Circle key={i} active color={colors[i]} />
+        ) : (
+          <Circle key={i} />
+        )
+      )}
+    </Wrapper>
+  );
+};
+
+PaginationSlider.propTypes = {
+  activeIndex: PropTypes.number,
+  length: PropTypes.number,
+  colors: PropTypes.array,
+};
+
+export default PaginationSlider;
